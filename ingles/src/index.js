adc06a446e45184a5b435a840212e7e19ae10e9a
Jesus Torres Alvarez <msc_jtorres2024@accitesz.com>
2026-02-02 15:00:37 -0600

    Trigger deploy: Aplicar fix de variables de entorno

--- ingles/src/index.js
+++ ingles/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// Build: 2026-02-02 - Fix variables de entorno Azure
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
