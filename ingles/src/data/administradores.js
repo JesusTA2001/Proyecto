9fcd86686365c907c6fd11560856dbf5fbbb3de8
Jesus Torres Alvarez <msc_jtorres2024@accitesz.com>
2026-07-18 11:46:22 -0600

    limpieza de archivos sin utilizar

--- ingles/src/data/administradores.js
+++
@@ -1,8 +0,0 @@
-export const initialAdministradores = [
-    { numero_empleado: "ADM01", nombre: "Jesus Torres Alvarez", correo: "Jesus123@teczamora.mx", curp: "TOAJ010607HMNRLSA9", telefono: "3515550101", direccion: "Oficina Principal", estado: "Activo" },
-    { numero_empleado: "ADM02", nombre: "Roberto Morales Gil", correo: "roberto.morales@teczamora.mx", curp: "MOGR801110HGRMNA02", telefono: "3515550102", direccion: "Recursos Humanos", estado: "Activo" },
-    { numero_empleado: "ADM03", nombre: "Karla Sánchez Peña", correo: "laura.sanchez@teczamora.mx", curp: "SAPL850220MGRMNA01", telefono: "3515550101", direccion: "Oficina Principal", estado: "Activo" },
-    { numero_empleado: "ADM04", nombre: "Ismael Morales Gil", correo: "roberto.morales@teczamora.mx", curp: "MOGR801110HGRMNA02", telefono: "3515550102", direccion: "Recursos Humanos", estado: "Activo" },
-    { numero_empleado: "ADM05", nombre: "Danica Sánchez Peña", correo: "laura.sanchez@teczamora.mx", curp: "SAPL850220MGRMNA01", telefono: "3515550101", direccion: "Oficina Principal", estado: "Activo" },
-    { numero_empleado: "ADM06", nombre: "Antonio Morales Gil", correo: "roberto.morales@teczamora.mx", curp: "MOGR801110HGRMNA02", telefono: "3515550102", direccion: "Recursos Humanos", estado: "Activo" },
-];
--- ingles/src/data/modalidad.js
+++
@@ -1,7 +0,0 @@
-export const initialModalidades = [
-  { id: 'MAT01',nombre: 'Matutino', descripcion: 'Clases por la mañana a Estudiantes que estudian en modalidad Vespertina, avanzando 1 nivel por semestre.' },
-  { id: 'VES02', nombre: 'Vespertino', descripcion: 'Clases por la tarde a Estudiantes que estudian en modalidad Matutina, avanzando 1 nivel por semestre.' },
-  { id: 'SAB03', nombre: 'Sabatino', descripcion: 'Clases intensivas impartidas únicamente los Sabados, avanzando 1 nivel por semestre.' },
-  { id: 'DOM04', nombre: 'Dominical', descripcion: 'Clases intensivas impartidas únicamente los Domingos, avanzando 1 nivel por semestre.' },
-  { id: 'INT05', nombre: 'Intensivo', descripcion: 'Clases de lunes a viernes 2 horas al dia, avanzando 2 niveles en 1 semestre.' },
-];
