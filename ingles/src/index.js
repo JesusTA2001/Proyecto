0c6e716d81ead6e5cb86b3fc8fd9257d6911a8c6
Jesus Torres Alvarez <msc_jtorres2024@accitesz.com>
2026-07-24 15:26:05 -0600

    Security: Disable console.log in production

--- ingles/src/index.js
+++ ingles/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 // Build: 2026-02-02 - Fix variables de entorno Azure
 
+// Deshabilitar console.log y console.info en producción por seguridad
+if (process.env.NODE_ENV === 'production') {
+  console.log = () => {};
+  console.info = () => {};
+  console.debug = () => {};
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
